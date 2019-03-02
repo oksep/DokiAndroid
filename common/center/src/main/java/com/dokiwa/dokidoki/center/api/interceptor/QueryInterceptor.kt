fb67bce37350f90dd90af1ff1ae3ed50b12e0cdf
Septenary <seven__up@sina.cn>
2019-03-02 18:31:13 +0800

    feat: QueryInterceptor multi-part body 签算

--- common/center/src/main/java/com/dokiwa/dokidoki/center/api/interceptor/QueryInterceptor.kt
+++ common/center/src/main/java/com/dokiwa/dokidoki/center/api/interceptor/QueryInterceptor.kt
@@ -3,13 +3,18 @@ package com.dokiwa.dokidoki.center.api.interceptor
 import com.dokiwa.dokidoki.center.ext.sha1
 import okhttp3.FormBody
 import okhttp3.Interceptor
+import okhttp3.MultipartBody
 import okhttp3.Response
+import okio.Buffer
 import java.net.URLEncoder
-import java.util.*
+import java.nio.charset.Charset
+import java.util.UUID
 
 /**
  * Created by Septenary on 2018/11/4.
  */
+private val UTF8 = Charset.forName("UTF-8")
+
 class QueryInterceptor(
     private var apiKey: String,
     private var apiSecret: String,
@@ -49,8 +54,27 @@ class QueryInterceptor(
             list
         } ?: mutableListOf()
 
+        // multi-part
+        val multiPartBodyList = (chain.request().body() as? MultipartBody)?.run {
+            val list = mutableListOf<Pair<String, String>>()
+            for (i in 0 until this.size()) {
+                val part = this.part(i)
+                val name = part.headers()
+                    ?.get("Content-Disposition")
+                    ?.split("; ")
+                    ?.find { it.startsWith("name=") && !it.startsWith("name=\"file\"") }
+                if (name != null && part.body().contentType() == null) {
+                    val buffer = Buffer()
+                    part.body().writeTo(buffer)
+                    val string = buffer.readString(UTF8)
+                    list.add(Pair("type", string))
+                }
+            }
+            list
+        } ?: mutableListOf()
+
         // sorted unSign parts
-        val sortedUnSignParts = (urlQueryList + formBodyList).sortedBy { it.first }
+        val sortedUnSignParts = (urlQueryList + formBodyList + multiPartBodyList).sortedBy { it.first }
 
         // sign
         val sign = sortedUnSignParts.joinToString("&") {
