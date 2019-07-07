35288b3fa79d0ecd10821d58e85714e60abdd1a6
Septenary <seven__up@sina.cn>
2019-07-07 21:59:47 +0800

    fix: timeline expandtextview toogle blind

--- feature/timeline/src/main/java/com/dokiwa/dokidoki/timeline/widget/TimelineTextView.kt
+++ feature/timeline/src/main/java/com/dokiwa/dokidoki/timeline/widget/TimelineTextView.kt
@@ -6,8 +6,6 @@ import android.view.View
 import android.widget.LinearLayout
 import android.widget.TextView
 import com.dokiwa.dokidoki.timeline.R
-import com.dokiwa.dokidoki.ui.ext.onceLayoutThen
-import com.dokiwa.dokidoki.ui.ext.zoomTouchArea
 import com.dokiwa.dokidoki.ui.view.ExpandableTextView
 
 /**
@@ -17,29 +15,52 @@ class TimelineTextView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : LinearLayout(context, attrs, defStyleAttr) {
 
-    private val expandableTextView: ExpandableTextView
-    private val toggle: TextView
+    private val expandableTextView: ExpandableTextView?
+    private val toggleView: TextView?
 
     init {
         View.inflate(context, R.layout.view_content_text, this)
         orientation = VERTICAL
-        expandableTextView = this.findViewById(R.id.expandableTextView)
-        toggle = this.findViewById(R.id.toggle)
-        if (isInEditMode) {
-            setText(this.expandableTextView.text?.toString())
+        expandableTextView = getChildAt(0) as? ExpandableTextView
+        toggleView = getChildAt(1) as? TextView
+    }
+
+    fun setText(content: String?) {
+        expandableTextView?.text = content
+    }
+
+    private fun setUpToggleView(isEllipsize: Boolean) {
+        val toggleView = this.toggleView ?: return
+        val expandableTextView = this.expandableTextView ?: return
+        if (isEllipsize) {
+            toggleView.visibility = View.VISIBLE
+            toggleView.setText(R.string.ui_expandable_text_view_expand)
+            toggleView.setOnClickListener {
+                if (expandableTextView.isExpanded) {
+                    expandableTextView.collapse()
+                    toggleView.setText(R.string.ui_expandable_text_view_expand)
+                } else {
+                    expandableTextView.expand()
+                    toggleView.setText(R.string.ui_expandable_text_view_collapse)
+                }
+            }
+        } else {
+            toggleView.visibility = View.GONE
         }
     }
 
-    fun setText(text: String?) {
-        if (text.isNullOrEmpty()) {
-            this.visibility = View.GONE
-        } else {
-            this.visibility = View.VISIBLE
-            this.expandableTextView.maxLines = 2
-            this.expandableTextView.setText(text, toggle)
-            this.toggle.onceLayoutThen {
-                it.zoomTouchArea(this)
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+
+        val expandableTextView = this.expandableTextView ?: return
+        if (expandableTextView.maxLines > 0) {
+            val isEllipsize = try {
+                expandableTextView.layout.getEllipsisCount(expandableTextView.layout.lineCount - 1) > 0
+            } catch (e: Exception) {
+                e.printStackTrace()
+                false
             }
+            setUpToggleView(isEllipsize)
         }
     }
 }
